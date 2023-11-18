24dc11c8fd6c387f0806448eebb6f4b4066095d6
Dull04 <abdulazizfebriansyah@gmail.com>
2023-11-18 12:20:40 +0700

    add Invoice and Customer model

--- src/main/java/org/cashdash/models/Customer.java
+++ src/main/java/org/cashdash/models/Customer.java
@@ -1,4 +1,33 @@
 package org.cashdash.models;
 
 public class Customer {
+    String nama;
+    String PhoneNumber;
+    
+    public Costumer(String nama, String Phone){
+        this.nama = nama;
+        this.PhoneNumber = Phone;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+    
+    
+
+
+
+    
 }
--- src/main/java/org/cashdash/models/Invoice.java
+++ src/main/java/org/cashdash/models/Invoice.java
@@ -1,4 +1,51 @@
 package org.cashdash.models;
 
 public class Invoice {
+    String id;
+    String method;
+    double amount;
+    boolean status;
+    
+    public Invoice(String method, double amount){
+        this.method = method;
+        this.amount = amount;
+    
+    }
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
+    
+    
 }
