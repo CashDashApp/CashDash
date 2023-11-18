3abc5174fe9d1355f34ec3cae561d7000c76104a
abdullahnz <edfallennzm@gmail.com>
2023-11-18 11:23:30 +0700

    add database connection helper and hash function

--- src/main/java/org/cashdash/database/Database.java
+++ src/main/java/org/cashdash/database/Database.java
@@ -1,4 +1,50 @@
 package org.cashdash.database;
 
+import java.sql.*;
+
 public class Database {
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/cashdash";
+    private static final String dbUsername = "root";
+    private static final String dbPassword = "root";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (Exception exception) {
+            return null;
+        }
+
+        return connection;
+    }
+
+    public static ResultSet executeQuery(String... args) {
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(args[0]);
+
+            for (int i = 1; i < args.length; i++)
+                statement.setString(i, args[i]);
+
+            return statement.executeQuery();
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+
+    public static int executeUpdate(String... args) {
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(args[0]);
+
+            for (int i = 1; i < args.length; i++)
+                statement.setString(i, args[i]);
+
+            return statement.executeUpdate();
+
+        } catch (Exception exception) {
+            return 0;
+        }
+    }
 }
--- src/main/java/org/cashdash/utils/Hash.java
+++ src/main/java/org/cashdash/utils/Hash.java
@@ -1,4 +1,13 @@
 package org.cashdash.utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class Hash {
+    public static String password(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean verify(String password, String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
 }
