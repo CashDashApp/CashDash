55600a4c1adb9e5272d76648428ad2a1b3a54e1f
macbook air <macbookair@MacBook-Air-macbook.local>
2023-11-18 11:56:18 +0700

    add item model

--- src/main/java/org/cashdash/models/Item.java
+++ src/main/java/org/cashdash/models/Item.java
@@ -1,4 +1,37 @@
 package org.cashdash.models;
 
-public class Item {
+public abstract class Item {
+    private String nama;
+    private Integer id;
+    private Integer stock;
+
+    public Item(String nama, Integer id, Integer stock){
+        this.nama = nama;
+        this.id = id;
+        this.stock = stock;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
