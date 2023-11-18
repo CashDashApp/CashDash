2a5ee5c4c440604ee6e93d722ad6ad8201af12a1
Andhika2104 <anggandhika.rdg@gmail.com>
2023-11-18 12:23:52 +0700

    add equipment model

--- src/main/java/org/cashdash/models/Equipment.java
+++ src/main/java/org/cashdash/models/Equipment.java
@@ -1,4 +1,44 @@
 package org.cashdash.models;
 
-public class Equipment {
+import java.time.LocalDate;
+
+public class Equipment extends Item {
+    private String type;
+    private LocalDate datein;
+    private String condtion;
+
+    public Equipment(String type, LocalDate datein, String condtion, String name, Integer stock) {
+        super(name, stock);
+        this.type = type;
+        this.datein = datein;
+        this.condtion = condtion;
+    }
+
+    public Equipment(String name, Integer stock) {
+        super(name, stock);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDatein(LocalDate datein) {
+        this.datein = datein;
+    }
+
+    public void setCondtion(String condtion) {
+        this.condtion = condtion;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getDatein() {
+        return datein;
+    }
+
+    public String getCondtion() {
+        return condtion;
+    }
 }
