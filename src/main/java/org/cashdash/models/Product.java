8070e8a4a4103249e146fa83fd723b50aad452eb
arlasfn <putrisifhana17@gmail.com>
2023-11-18 12:29:48 +0700

    add product and category model

--- src/main/java/org/cashdash/models/Product.java
+++ src/main/java/org/cashdash/models/Product.java
@@ -1,4 +1,51 @@
 package org.cashdash.models;
 
-public class Product {
+public class Product extends Item {
+    private Category category;
+    private String description;
+    private String imageUrl;
+    private double price;
+
+    public Product(String name, String description, Integer stock) {
+        super(name, stock);
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public void getInfo() {
+        
+    }
+
+    
 }
--- src/main/java/org/cashdash/views/LoginForm.java
+++
@@ -1,4 +0,0 @@
-package org.cashdash.views;
-
-public class LoginForm {
-}
